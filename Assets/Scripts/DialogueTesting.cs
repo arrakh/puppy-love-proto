66ebf87e34a550ded2e6e04a522c5b9902dc5f8a
Arya Rakha <arrakhpra@gmail.com>
2025-02-26 11:46:10 +0100

    Dialogue Testing

---
+++ Assets/Scripts/DialogueTesting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using Ink;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DialogueTesting : MonoBehaviour
+    {
+        public InkStoryController storyController;
+        public TextAsset storyInk;
+        public string startPath = "morning";
+
+        private IEnumerator Start()
+        {
+            storyController.InitializeStory(storyInk.text);
+            yield return storyController.StartStory(startPath);
+        }
+    }
+}
